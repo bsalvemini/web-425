d605f34fd8a7197b02814c596042abc7a5c33700
bsalvemini <bcsalvemini@gmail.com>
2024-10-02 11:03:17 -0400

    Add work for Assignment 8.2

--- rpg-character-builder/src/app/character-faction/character-faction.component.spec.ts
+++ rpg-character-builder/src/app/character-faction/character-faction.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharacterFactionComponent } from './character-faction.component';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('CharacterFactionComponent', () => {
   let component: CharacterFactionComponent;
   let fixture: ComponentFixture<CharacterFactionComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CharacterFactionComponent]
-    })
-    .compileComponents();
+      imports: [CharacterFactionComponent, HttpClientTestingModule],
+    }).compileComponents();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
 
     fixture = TestBed.createComponent(CharacterFactionComponent);
     component = fixture.componentInstance;
@@ -19,5 +25,60 @@ describe('CharacterFactionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should handle errors when data is not found', () => {
+    const expectedMessage = 'Not found';
+    const req = httpTestingController.expectOne(
+      'http://localhost:3000/api/character-factions'
+    );
+    req.flush('Not found', { status: 404, statusText: 'Not Found' });
+    expect(component.errorMessage).toEqual(expectedMessage);
+  });
+
+  it('should correctly fetch a list of character factions', () => {
+    const mockCharFactionData = [
+      {
+        name: 'Faction 1',
+        description: 'Faction 1 description',
+      },
+      {
+        name: 'Faction 2',
+        description: 'Faction 2 description',
+      },
+    ];
+
+    const req = httpTestingController.expectOne(
+      'http://localhost:3000/api/character-factions'
+    );
+    req.flush(mockCharFactionData);
+
+    expect(component.charFactions).toEqual(mockCharFactionData);
+  });
+
+  it('should update the characterFactions div when character factions are found', () => {
+    const mockCharFactionData = [
+      {
+        name: 'Faction 1',
+        description: 'Faction 1 description',
+      },
+      {
+        name: 'Faction 2',
+        description: 'Faction 2 description',
+      },
+    ];
+
+    const req = httpTestingController.expectOne(
+      'http://localhost:3000/api/character-factions'
+    );
+    req.flush(mockCharFactionData);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement; // Get the compiled HTML of the component
+    const charFactionRows = compiled.querySelectorAll('tbody > tr'); // Get all the character faction rows from the table
+
+    // Expect the number of rows to equal to the number of character factions
+    expect(charFactionRows.length).toEqual(component.charFactions.length);
   });
 });
--- rpg-character-builder/src/app/character-faction/character-faction.component.ts
+++ rpg-character-builder/src/app/character-faction/character-faction.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,12 +6,80 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      character-faction works!
-    </p>
+    <h1>Character Faction</h1>
+    @if (charFactions) {
+    <div class="characterFactions">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for (faction of charFactions; track faction) {
+          <tr>
+            <td>{{ faction.name }}</td>
+            <td>{{ faction.description }}</td>
+          </tr>
+          }
+        </tbody>
+      </table>
+    </div>
+    } @else if(errorMessage) {
+    <div class="characterFactions">
+      <p class="error">Server down</p>
+    </div>
+    }
   `,
-  styles: ``
+  styles: `
+.error {
+  font-size: x-large;
+  font-weight: bold;
+  color: red;
+  }
+
+  table {
+  border: 1px solid #bb6528;
+  border-collapse: collapse;
+  width: 100%;
+  margin-top: 10px;
+  }
+
+  th,
+  td {
+    border: 1px solid #bb6528;
+  }
+
+  td {
+    text-align: center;
+  }
+
+  th {
+    background-color: #bb6528;
+    color: #fff;
+  }
+
+  tr:nth-child(even) {
+    background-color: #ffebcd;
+  }
+
+  `,
 })
 export class CharacterFactionComponent {
+  charFactions: any;
+  errorMessage: string = '';
 
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:3000/api/character-factions').subscribe({
+      next: (res) => {
+        console.log(res);
+        this.charFactions = res;
+      },
+      error: (err) => {
+        console.log('err: ', err);
+        this.errorMessage = err.error;
+      },
+    });
+  }
 }
